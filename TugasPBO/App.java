478b96c91c8383d5f806e9423f429417f7061e6d
Fadli Hidayat <rajoparmatoalam@gmail.com>
2023-11-26 20:17:02 +0700

    Tes1

--- TugasPBO/App.java
+++ TugasPBO/App.java
@@ -1,6 +1,10 @@
 public class App {
-    
     public static void main(String[] args) {
+        Member member1=new MemberReguler(10);
+        
+        System.out.println("Poin member1 adalah : " + member1.getPoin());
+
+
     }
     
 }
