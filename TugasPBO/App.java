5f2a86afdedb6b73c934b6105ebbd248ac6a3f55
Fadli Hidayat <rajoparmatoalam@gmail.com>
2023-12-03 12:21:49 +0700

    tugas3

--- TugasPBO/App.java
+++ TugasPBO/App.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        Member member1=new MemberReguler(10);
+        //Member member1=new MemberReguler(10);
         //penerapan pholymorphism dengan inisiasi MemberReguler sebagai Member
         //System.out.println("");
         //System.out.println("Poin member1 adalah : " + member1.getPoin());
